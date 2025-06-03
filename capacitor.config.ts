e52eb0a743a022fed9fcdd6b440dcbc923e79a10
Steven Mattmann <steven_mattmann@sluz.ch>
2025-06-03 10:59:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'schnitzeljagd_steven_yves',
+  webDir: 'www'
+};
+
+export default config;
