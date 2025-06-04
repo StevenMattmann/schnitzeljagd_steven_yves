adadb6d8853c7c01828a8e46195d8db0adac90fd
Steven Mattmann <steven_mattmann@sluz.ch>
2025-06-04 13:38:27 +0200

    feat: add qr-code logic

--- src/app/pages/qrcode/qrcode.page.ts
+++ src/app/pages/qrcode/qrcode.page.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { TrackingService } from '../../services/tracking.service';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-qrcode',
   templateUrl: './qrcode.page.html',
   styleUrls: ['./qrcode.page.scss'],
+  imports: [
+    IonicModule
+  ],
+  standalone: true
 })
-export class QrcodePage {
+export class QRCodePage implements OnInit {
+  scannerResult: string = 'No Data...';
+  private startTime: number | null = null;
+  private taskCompleted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private trackingService: TrackingService
+  ) {}
 
+  ngOnInit() {
+    this.startTask();
+  }
+
+  startTask() {
+    this.startTime = Date.now();
+  }
+
+  async scanBarcode() {
+    try {
+      await BarcodeScanner.checkPermission({ force: true });
+      await BarcodeScanner.hideBackground();
+      const result = await BarcodeScanner.startScan();
+
+      this.scannerResult = result?.hasContent ? result.content ?? 'No Data...' : 'Kein Inhalt';
+      if (this.scannerResult === 'M335@ICT-BZ' && !this.taskCompleted) {
+        this.taskCompleted = true;
+        await Haptics.impact({ style: ImpactStyle.Medium });
+        console.log('✔️ Richtiger QR-Code gescannt');
+      }
+    } catch (error) {
+      console.error('QR-Code Scan fehlgeschlagen:', error);
+      this.scannerResult = 'Fehler beim Scan';
+    } finally {
+      await BarcodeScanner.showBackground();
+      await BarcodeScanner.stopScan();
+    }
+  }
+
+  skipTask() {
+    this.router.navigate(['/power']);
+  }
+
+  completeTask() {
+    if (this.taskCompleted && this.startTime) {
+      const taskDuration = Date.now() - this.startTime;
+      this.trackingService.addTask('QRCode', taskDuration);
+      this.router.navigate(['/power']);
+    }
+  }
+
+  cancelTask() {
+    this.router.navigate(['/leaderboard']);
+    this.scannerResult = '';
+  }
 }
