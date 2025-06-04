b0f32f4b89037ec03ae1038c56bdd84d3c9e3b3e
YvesTroxler1 <yves.troxler@gmx.ch>
2025-06-04 12:29:10 +0200

    added requiredDistance

--- src/app/pages/geolocation/geolocation.page.ts
+++ src/app/pages/geolocation/geolocation.page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonFooter} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-geolocation',
   templateUrl: './geolocation.page.html',
   styleUrls: ['./geolocation.page.scss'],
+  imports: [
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    IonFooter,
+    IonButton
+  ]
 })
 export class GeolocationPage {
 
