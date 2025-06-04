721612f6cb3582690cd773010078d2c8bdf8e0cd
YvesTroxler1 <yves.troxler@gmx.ch>
2025-06-04 09:14:41 +0200

    added giveUp modal

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,19 +1,42 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {IonTabs, IonTabBar, IonTabButton, IonButton} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
+import {AlertController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonButton],
   standalone: true
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
+  constructor(private alertController: AlertController, private router: Router) {
     addIcons({ triangle, ellipse, square });
+  }
+
+  async presentGiveUpAlert() {
+    const alert = await this.alertController.create({
+      header: 'Spiel aufgeben?',
+      message: 'Willst du das Spiel wirklich aufgeben?',
+      buttons: [
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+        },
+        {
+          text: 'Ja, aufgeben',
+          handler: () => {
+            this.router.navigateByUrl('/');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
