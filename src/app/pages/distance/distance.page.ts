08655f57002f8462b0793c3a57da4e6cbafcb585
YvesTroxler1 <yves.troxler@gmx.ch>
2025-06-05 10:56:18 +0200

    used ngZone

--- src/app/pages/distance/distance.page.ts
+++ src/app/pages/distance/distance.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, signal } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   IonButton,
